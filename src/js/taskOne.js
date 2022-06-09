1e9cda227b7bb4b2683ebb74dd5b5695f467e75f
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-06-09 20:20:03 +0200

    task one change

--- src/js/taskOne.js
+++ src/js/taskOne.js
@@ -2,11 +2,8 @@ export default function charProperties(char, sortParam) {
   const keys = Object.keys(char);
   const nameOrderedKeys = keys.filter((key) => sortParam.includes(key));
   const alphabetOrderedKeys = keys.filter((key) => !sortParam.includes(key)).sort();
-  return nameOrderedKeys.concat(alphabetOrderedKeys).map((key) => {
-    const object = {
-      key,
-      value: char[key],
-    };
-    return object;
-  });
+  return nameOrderedKeys.concat(alphabetOrderedKeys).map((key) => ({
+    key,
+    value: char[key],
+  }));
 }
